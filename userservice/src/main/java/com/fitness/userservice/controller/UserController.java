5b509c658424f52e733d2eb9cf6b159e5a1fa2b9
Diwaker <diwakersinghrj06@gmail.com>
2026-05-30 01:50:12 +0530

    User Registed

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.fitness.userservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fitness.userservice.services.UserService;
+
+import jakarta.validation.Valid;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
 
 import lombok.AllArgsConstructor;
 
@@ -16,7 +23,9 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.register(request));
+    }
 
      
 
--- userservice/src/main/java/com/fitness/userservice/services/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -2,7 +2,45 @@ package com.fitness.userservice.services;
 
 import org.springframework.stereotype.Service;
 
+import com.fitness.userservice.UserRepository;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService {
+    
+    private final UserRepository userRepository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+
+            throw new RuntimeException("Email already exists");
+        }
+
+        // Implementation for user registration
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        User savedUser = userRepository.save(user);
+        UserResponse userResponse = new UserResponse();
+
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+        
+        return userResponse;
+    }
 
 }
