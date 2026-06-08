89aefbb16ed4b4029d0e6e04729da0465bef655d
Diwaker <diwakersinghrj06@gmail.com>
2026-06-08 19:53:46 +0530

    Processed Recommendation and saved into MongoDB Database

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -4,6 +4,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ActivityMessageListener {
 
     private final ActivityAIService activityAIService;  // Injecting ActivityAIService to use its functionality for processing activity data and generating AI recommendations
+    private final RecommendationRepository recommendationRepository;  // Injecting RecommendationRepository to save the generated recommendations to the database
 
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
     public void processActivity(Activity activity) {
@@ -21,7 +24,8 @@ public class ActivityMessageListener {
         // Here you can add logic to process the activity data, such as saving it to a database,
         // performing analysis, or triggering other actions based on the activity type or metrics.
 
-        activityAIService.generateRecommendations(activity);  // Call the method to generate AI recommendations based on the received activity data
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);  // Call the method to generate AI recommendations based on the received activity data
+        recommendationRepository.save(recommendation);
     }
 
 }
